0c66ae390ac27e2342c5096710ab98f70006c060
batista7557 <gabriel.bat2001@gmail.com>
2023-01-19 22:41:21 -0300

    Criando menu

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Usuarios from './components/Usuarios/Usuarios'
 function App() {
   return (
     <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li><a href='/'>Início</a></li>
+            <li><a href='/usuarios'>Usuários cadastrados</a></li>
+            <li><a href='/adicionar'>Adicionar usuários</a></li>
+          </ul>
+        </nav>
+      </header>
       <main>
         <Usuarios />
       </main>

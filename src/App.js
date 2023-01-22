650b1186317482c74447285281172d2e6e77ad1d
batista7557 <gabriel.bat2001@gmail.com>
2023-01-21 23:57:40 -0300

    Rotas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 
 import Usuarios from './components/Usuarios/Usuarios'
+import AdicionarUsuario from './components/AdicionarUsuario/AdicionarUsuario';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <nav>
-          <ul>
-            <li><a href='/'>Início</a></li>
-            <li><a href='/usuarios'>Usuários cadastrados</a></li>
-            <li><a href='/adicionar'>Adicionar usuários</a></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <Usuarios />
-      </main>
-    </div>
+    <Router>     
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Início</Link>
+              </li>
+              <li>
+                <Link href="/usuarios">Usuários cadastrados</Link>
+              </li>
+              <li>
+                <Link href="/adicionar">Adicionar Usuário</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Switch>
+            <Route path="/usuarios">
+              <Usuarios/>
+            </Route>
+            <Route path="/adicionar">
+              <AdicionarUsuario/>
+            </Route>
+            <Route path="/">
+              <Home/>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
+
 }
 
 export default App;
